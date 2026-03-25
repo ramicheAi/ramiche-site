2be80e6b4d670213ac9d17f53782a0b8fea3092f
Ramon Walton <ramichehq@gmail.com>
2026-03-25 11:29:09 -0400

    fix: nerve center iframe path (was pointing to wrong directory name)

--- src/app/command-center/nerve-center/page.tsx
+++ src/app/command-center/nerve-center/page.tsx
@@ -1,7 +1,7 @@
 export default function NerveCenterPage() {
   return (
     <iframe
-      src="/yolo-builds/2026-03-17-proximon-nerve-centre/index.html"
+      src="/yolo-builds/2026-03-17-nerve-center/index.html"
       style={{
         width: "100%",
         height: "100vh",
